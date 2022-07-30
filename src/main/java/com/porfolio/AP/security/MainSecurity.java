08dde5d984a1985c797e9bdd23bd7ea5d2a94467
leonelrocchia <leonelrocchia@gmail.com>
2022-07-30 10:59:38 -0300

    borramos el auth

--- src/main/java/com/porfolio/AP/security/MainSecurity.java
+++ src/main/java/com/porfolio/AP/security/MainSecurity.java
@@ -55,7 +55,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
-                .authorizeRequests().antMatchers("/auth/**").permitAll()
+                .authorizeRequests().antMatchers("**").permitAll()
                 .anyRequest().authenticated().and().exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
